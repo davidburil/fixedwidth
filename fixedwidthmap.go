96c1c2d33c81ea07f6891c72cd1e6424d25ee2be
David Buril <davidmsd@gmail.com>
2019-09-19 00:48:08 -0300

    Ajustes na estrutura dos metodos e implementação da extração da amostra.
    - Melhoria no método que verifica se o caracter é um espaço em branco;
    - Implementação do método que extrai os dados de amostra;

--- fixedwidthmap.go
+++ fixedwidthmap.go
@@ -1,18 +1,56 @@
 package fixedwithmap
 
-// ByteToBit converte um array de byte em um array de booleano.
-func ByteToBit(data []byte) (r []bool) {
+import (
+	"bufio"
+	"io"
+	"os"
+	"unicode"
+)
 
-	var bits []bool
+// InferColumnsIndex Metodo publico para fazer a inferência o index das colunas.
+func InferColumnsIndex(fileName string, sampleValue int) {
 
-	for _, b := range data{
-		bits = append(bits, spaceZero(b))
-	}
+	dataSampleFromFile := extractDataSamplefromFile(fileName, sampleValue)
 
-	return bits
+	print(dataSampleFromFile)
+
 }
 
-// spaceZero retorna false se o byte for igual a um espaço em branco.
-func spaceZero(b byte) bool{
-	return b != 32
+// extractDataSamplefromFile Extrai os dados de amostra do arquivo convertido para uma matrix de boleano.
+func extractDataSamplefromFile(fileName string, sampleValue int) map[int][]bool {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	dataSample := make(map[int][]bool)
+
+	reader := bufio.NewReader(file)
+
+	for i := 0; i < sampleValue; i++ {
+		line, _, err := reader.ReadLine()
+
+		if err == io.EOF {
+			break
+		}
+		dataSample[i] = convertArrayByteToArrayBool(line)
+	}
+	return dataSample
+}
+
+// convertArrayByteToArrayBool converte um array de byte em um array de booleano.
+func convertArrayByteToArrayBool(data []byte) []bool {
+
+	var r []bool
+	for _, b := range data {
+		r = append(r, isNonWhiteSpace(b))
+	}
+	return r
+}
+
+// isNonWhiteSpace retorna true caso não seja um espaço em branco.
+func isNonWhiteSpace(b byte) bool {
+	return !unicode.IsSpace(rune(b))
 }
--- fixedwidthmap_test.go
+++ fixedwidthmap_test.go
@@ -1,26 +1,11 @@
 package fixedwithmap
 
 import (
-	"bufio"
-	"fmt"
-	"os"
 	"testing"
 )
 
 func TestReadFile(t *testing.T) {
 
-	file, err := os.Open("resources/dados2.txt")
+	InferColumnsIndex("resources/dados2.txt", 200)
 
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	reader := bufio.NewReader(file)
-
-	linha, _, _ := reader.ReadLine()
-
-	bits := ByteToBit(linha)
-
-	fmt.Println(bits)
 }
