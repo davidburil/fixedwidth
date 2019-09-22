a1c5202c654ec8978fdbe9e765cf046411f54c22
David Buril <davidmsd@gmail.com>
2019-09-22 00:53:23 -0300

    Simplificação da logica dos metodos e definição da licença de software.

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Pacote escrito em Go que verifica uma amostra de dados de um arquivo de texto e 
  - [x] Metodo para converter a linha do arquivo em uma matrix de boleano;
  - [x] Determinar algoritmo para a amostra de dados em uma matrix de boleano;
  - [x] Processar a matrix de bits e deterninar o mapeamento das colunas do arquivo;
+ - [x] Definir lincença de software livre;
  
  ## Links
   - https://dev.to/awwsmm/java-infer-column-widths-of-a-fixed-width-text-file-2hh0
--- fixedwidthmap.go
+++ fixedwidthmap.go
@@ -1,79 +1,109 @@
+/*
+   Copyright 2019 The David Buril Cardozo de Oliveira
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package fixedwithmap
 
 import (
 	"bufio"
+	"errors"
 	"io"
 	"os"
 	"unicode"
 )
 
 // InferColumnsIndex Metodo publico para fazer a inferência o index das colunas.
-func InferColumnsIndex(fileName string, sampleValue int) {
+func InferColumnsIndex(file *os.File, sampleValue uint) (columns []int, err error) {
 
-	dataSampleFromFile := extractDataSamplefromFile(fileName, sampleValue)
+	dataSampleFromFile, e := extractColumnArrayWhiteSpace(file, sampleValue)
 
-	line := mergeLinesFromMatrix(dataSampleFromFile)
+	if e != nil {
+		return nil, e
+	}
 
-	inferColumnsIndexMetodo1(line)
+	columns, e = inferColumnsIndexAlignLeft(dataSampleFromFile)
+
+	return columns, e
 
 }
 
-func inferColumnsIndexMetodo1(data []bool) []int {
+// extractColumnArrayWhiteSpace Metodo responsável por extrair um array com o resultado do processamento da amostra.
+func extractColumnArrayWhiteSpace(file *os.File, sampleValue uint) (columns []bool, err error) {
 
-	var ret []int
-	valorAnterior := false
-
-	for key, value := range data {
-		if value && !valorAnterior {
-			ret = append(ret, key)
-		}
-		valorAnterior = value
+	if file == nil {
+		return nil, errors.New("the file parameter is required")
 	}
-
-	return ret
-}
-
-func mergeLinesFromMatrix(matrix map[int][]bool) []bool {
-	ret := matrix[0]
-	for _, line := range matrix {
-		for i, column := range line {
-			ret[i] = ret[i] || column
-		}
-	}
-	return ret
-}
-
-// extractDataSamplefromFile Extrai os dados de amostra do arquivo convertido para uma matrix de boleano.
-func extractDataSamplefromFile(fileName string, sampleValue int) map[int][]bool {
-	file, err := os.Open(fileName)
-
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	dataSample := make(map[int][]bool)
 
 	reader := bufio.NewReader(file)
 
-	for i := 0; i < sampleValue; i++ {
-		line, _, err := reader.ReadLine()
+	for i := uint(0); i < sampleValue || sampleValue == 0; i++ {
+		line, _, e := reader.ReadLine()
 
-		if err == io.EOF {
+		if e != nil && e != io.EOF {
+			return nil, e
+		}
+
+		if e == io.EOF {
 			break
 		}
-		dataSample[i] = convertArrayByteToArrayBool(line)
+
+		if columns == nil {
+			columns = make([]bool, len(line))
+		}
+
+		for len(columns) < len(line) {
+			columns = append(columns, false)
+		}
+
+		for key, value := range line {
+			columns[key] = columns[key] || isNonWhiteSpace(value)
+		}
+
 	}
-	return dataSample
+
+	rewindFile(file)
+
+	return columns, nil
 }
 
-// convertArrayByteToArrayBool converte um array de byte em um array de booleano.
-func convertArrayByteToArrayBool(data []byte) []bool {
-	var r []bool
-	for _, b := range data {
-		r = append(r, isNonWhiteSpace(b))
+// rewindFile Metodo responsavel por voltar o ponteiro para o inicio do arquivo.
+func rewindFile(file *os.File) {
+	file.Seek(0, 0)
+}
+
+// inferColumnsIndexAlignLeft Metodo responsável por inferir as posicoes das colunas alinhadas a esquerda.
+func inferColumnsIndexAlignLeft(data []bool) (columns []int, err error) {
+
+	if data == nil || len(data) < 4 {
+		return nil, errors.New("the data parameter is required and must have at least 4 elements")
 	}
-	return r
+
+	beforeValue := false
+
+	for key, value := range data {
+		if value && !beforeValue {
+			columns = append(columns, key)
+		}
+		beforeValue = value
+	}
+
+	if columns == nil {
+		return nil, errors.New("unable to identify columns")
+	}
+
+	return columns, nil
 }
 
 // isNonWhiteSpace retorna true caso não seja um espaço em branco.
